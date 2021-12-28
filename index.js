ae6d2864076c33a88d28db53caaf4f8747b5a2bb
Ramanan S <rams2@publicisgroupe.net>
2021-12-28 20:02:16 +0530

    connected between services

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const axios = require("axios");
 
 const app = express();
 app.use(bodyParser());
 
-app.post('/events',(req, res) => {
+app.post('/events', async (req, res) => {
     console.log(req.body);
+    await axios.post(`http://localhost:4000/events`,req.body);
+    await axios.post(`http://localhost:5000/events`,req.body);
+
+
 });
 
 app.listen(6000, ()=> {
